6c564d106bbcdb802c08439e21d0a5caa31b09d7
Ariel Crivellaro Martins <ariel.martins@kroton.com.br>
2022-05-13 14:33:24 -0300

    chore: finish test

---
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,30 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from '../../dtos';
+import { User } from '../../entities/User';
+import { IUsersRepository } from '../IUsersRepository';
+
+export class UsersRepository implements IUsersRepository {
+  
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async findUserWithGamesById({user_id,}: IFindUserWithGamesDTO): Promise<User> {
+    // Complete usando ORM
+    const user = await this.repository.findOneOrFail(user_id,{ relations: ["games"] });
+    return user;
+  }
+
+ findAllUsersOrderedByFirstName(): Promise<User[]> {
+    return this.repository.query('SELECT * FROM users AS u ORDER BY first_name'); // Complete usando raw query
+  }
+
+  async findUserByFullName({first_name, last_name,}: IFindUserByFullNameDTO): Promise<User[] | undefined> {
+     // Complete usando raw query
+    return this.repository.query("SELECT * FROM users WHERE first_name ILIKE $1 AND last_name ILIKE $2", [first_name, last_name]); 
+    
+  }
+}
